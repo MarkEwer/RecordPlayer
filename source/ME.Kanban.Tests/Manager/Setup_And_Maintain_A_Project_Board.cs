a5fd750d86249c6a530458dc80dd77ba5395bf5a
Mark Ewer <MEwer@paylocity.com>
2021-07-26 08:37:38 -0500

    cleanup

--- source/ME.Kanban.Tests/Manager/Setup_And_Maintain_A_Project_Board.cs
+++ source/ME.Kanban.Tests/Manager/Setup_And_Maintain_A_Project_Board.cs
@@ -39,52 +39,52 @@ namespace ME.Kanban.Tests.Manager
 
         private async Task And_a_user_named_Dave_has_logged_into_the_system()
         {
-            await Task.CompletedTask; return;
+            await Task.CompletedTask; 
         }
 
         private async Task And_Dave_is_the_only_team_member_in_the_new_project()
         {
-            await Task.CompletedTask; return;
+            await Task.CompletedTask; 
         }
 
         private async Task And_Dave_will_be_registered_as_a_team_member_with_the_Manager_role_on_the_project()
         {
-            await Task.CompletedTask; return;
+            await Task.CompletedTask;
         }
 
         private async Task And_it_will_have_the_description_Unit_Test()
         {
-            await Task.CompletedTask; return;
+            await Task.CompletedTask;
         }
 
         private async Task And_it_will_have_the_name_Unit_Test()
         {
-            await Task.CompletedTask; return;
+            await Task.CompletedTask;
         }
 
         private async Task And_supplies_a_project_description_of_Unit_Test()
         {
-            await Task.CompletedTask; return;
+            await Task.CompletedTask;
         }
 
         private async Task And_supplies_a_project_name_of_Unit_Test()
         {
-            await Task.CompletedTask; return;
+            await Task.CompletedTask;
         }
 
         private async Task Given_there_are_no_existing_projects_in_the_system()
         {
-            await Task.CompletedTask; return;
+            await Task.CompletedTask;
         }
 
         private async Task Then_a_new_project_will_be_created()
         {
-            await Task.CompletedTask; return;
+            await Task.CompletedTask;
         }
 
         private async Task When_the_user_chooses_the_Create_Project_option()
         {
-            await Task.CompletedTask; return;
+            await Task.CompletedTask;
         }
     }
 }
